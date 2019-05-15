fc1f99ab82910fe6c0ed5615bd3e5bc31cec6b1a
harishindegene <harish.pandalangatt@indegene.com>
2019-05-15 16:35:10 +0530

    My First Test Commit

---
+++ src/testpackage/test1/Demo1.java
@@ -0,0 +1,24 @@
+package testpackage.test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	
+	@Test
+	
+	public void openbrowser()
+	{
+		
+		System.setProperty("webdriver.chrome.driver","D:\\My Files\\Softwares & Files\\chromedriver_win32_1\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.tricentis.com/");
+		driver.manage().window().maximize();
+		driver.close();
+
+	
+
+}
+}
